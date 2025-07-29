a1b69941f9b66ca8aaa92d5f8550a7196a965d7d
فرحان حمود العنزي <falanazi@MOITP.local>
2025-07-29 18:04:56 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Material from "./Material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { orange, purple, green } from "@mui/material/colors";
+import Container from '@mui/material/Container';
+
+
+const theme = createTheme({
+  status: {
+    danger: orange[500],
+  },
+  palette: {
+    primary: {
+      main: green[500],
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App" >
+        <Material />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/Material.js
@@ -0,0 +1,50 @@
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import { deepOrange, deepPurple } from "@mui/material/colors";
+import Checkbox from "@mui/material/Checkbox";
+import Slider from "@mui/material/Slider";
+import Chip from "@mui/material/Chip";
+import Container from '@mui/material/Container';
+
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+export default function Material() {
+  return (
+     <Container maxWidth="sm">
+    <Stack direction="row" spacing={2} style={{ background: "gray" }}>
+      <Button
+        variant="contained"
+        style={{ background: "#009688", color: "white" }}
+        onClick={() => {
+          alert("clicked");
+        }}
+        color="primary"
+      >
+        primary
+      </Button>
+      <Button variant="contained" color="success" size="large">
+        Success
+      </Button>
+      <Button variant="outlined" color="error">
+        Error
+      </Button>
+      <Checkbox {...label} />
+      <Slider aria-label="Volume" />
+
+      <Stack direction="row" spacing={1}>
+        <Chip label="primary" color="primary" />
+        <Chip label="success" color="success" />
+      </Stack>
+    </Stack>
+
+    {/* // <Stack direction="row" spacing={2}>
+    //   <Avatar>H</Avatar>
+    //   <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
+    //   <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+    // </Stack> */}
+
+    </Container>
+  );
+}
